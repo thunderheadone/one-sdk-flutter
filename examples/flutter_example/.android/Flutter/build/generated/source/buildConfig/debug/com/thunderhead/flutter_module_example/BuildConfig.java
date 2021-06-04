3a5f1fe15199977e484f38feef7c53d8123a8659
Alex Nguyen <anguyen@thunderhead.com>
2021-06-04 11:51:24 -0600

    Add flutter_example project

---
+++ examples/flutter_example/.android/Flutter/build/generated/source/buildConfig/debug/com/thunderhead/flutter_module_example/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.thunderhead.flutter_module_example;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.thunderhead.flutter_module_example";
+  public static final String BUILD_TYPE = "debug";
+}

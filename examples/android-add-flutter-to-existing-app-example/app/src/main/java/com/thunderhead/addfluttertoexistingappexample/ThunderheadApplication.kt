f5ffa4a65a931ee6291f397598afa5c71737fd11
Alex Nguyen <anguyen@thunderhead.com>
2021-06-04 12:07:37 -0600

    Add ThunderheadApplication.kt

---
+++ examples/android-add-flutter-to-existing-app-example/app/src/main/java/com/thunderhead/addfluttertoexistingappexample/ThunderheadApplication.kt
@@ -0,0 +1,10 @@
+package com.thunderhead.addfluttertoexistingappexample
+
+import android.app.Application
+
+class ThunderheadApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //TODO: Configure ONE
+    }
+}

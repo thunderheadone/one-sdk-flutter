ba16e81c28f9e667ab79a0ef38bff544d0a8a509
Alex Nguyen <anguyen@thunderhead.com>
2022-01-20 14:52:54 -0600

    code cleanup

--- android/src/main/java/com/thunderhead/one_sdk_flutter/OnePlugin.java
+++ android/src/main/java/com/thunderhead/one_sdk_flutter/OnePlugin.java
@@ -40,225 +40,230 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** OnePlugin */
+/**
+ * OnePlugin
+ */
 public class OnePlugin implements MethodCallHandler, FlutterPlugin {
-  protected static final String LOG_TAG = "OnePlugin";
-  private MethodChannel methodChannel;
-  private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    protected static final String LOG_TAG = "OnePlugin";
+    private MethodChannel methodChannel;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-  /** Plugin registration. */
-  /** We are keeping the registerWith() method to remain compatible with apps that don’t use the v2 Android embedding */
-  @SuppressWarnings("deprecation")
-  public static void registerWith(Registrar registrar) {
-    final OnePlugin instance = new OnePlugin();
-    instance.onAttachedToEngine(registrar.context(), registrar.messenger());
-  }
-
-  @Override
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
-    onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
-  }
-
-  private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
-    methodChannel = new MethodChannel(messenger, "one_sdk_flutter");
-    methodChannel.setMethodCallHandler(this);
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding binding) {
-    methodChannel.setMethodCallHandler(null);
-    methodChannel = null;
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method.equals("sendInteraction")) {
-      final Map<String, Object> arguments = call.arguments();
-      String interactionPath = (String) arguments.get("interactionPath");
-      HashMap<String, Object> properties = (HashMap) arguments.get("properties");
-      try {
-        sendInteraction(interactionPath, properties, result);
-      } catch (Exception e) {
-        result.error("Failed to send interaction", e.getLocalizedMessage(), e);
-      }
-    } else if (call.method.equals("sendResponseCode")) {
-      final Map<String, String> arguments = call.arguments();
-      String responseCode = (String) arguments.get("responseCode");
-      String interactionPath = (String) arguments.get("interactionPath");
-      sendResponseCode(responseCode, interactionPath, result);
-    } else if (call.method.equals("setLogLevel")) {
-      setLogLevel(call, result);
-    } else if (call.method.equals("initializeOne")) {
-      initializeOne(call, result);
-    } else if (call.method.equals("optOut")) {
-      optOut(call, result);
-    } else {
-      result.notImplemented();
-    }
-  }
-
-  private void initializeOne(MethodCall call, final Result result) {
-    final Map<String, Object> arguments = call.arguments();
-    String siteKey = (String) arguments.get("siteKey");
-    String touchpointURI = (String) arguments.get("touchpoint");
-    String apiKey = (String) arguments.get("apiKey");
-    String sharedSecret = (String) arguments.get("sharedSecret");
-    String userId = (String) arguments.get("userID");
-    String host = (String) arguments.get("host");
-    boolean adminMode = (Boolean) arguments.get("adminMode");
-
-    final OneConfiguration oneConfiguration = new OneConfiguration.Builder()
-            .siteKey(siteKey)
-            .apiKey(apiKey)
-            .sharedSecret(sharedSecret)
-            .userId(userId)
-            .host(URI.create(host))
-            .touchpoint(URI.create(touchpointURI))
-            .mode(adminMode ? OneMode.ADMIN : OneMode.USER)
-            .build();
-    One.setConfiguration(oneConfiguration);
-    result.success(true);
-  }
-
-  private void setLogLevel(MethodCall call, final Result result) {
-    OneLoggingConfiguration oneLoggingConfiguration;
-    final Map<String, Boolean> arguments = call.arguments();
-    Boolean enabled = arguments.get("logLevel");
-
-    if (enabled == null) {
-      enabled = true;
+    /** Plugin registration. */
+    /**
+     * We are keeping the registerWith() method to remain compatible with apps that don’t use the v2 Android embedding
+     */
+    @SuppressWarnings("deprecation")
+    public static void registerWith(Registrar registrar) {
+        final OnePlugin instance = new OnePlugin();
+        instance.onAttachedToEngine(registrar.context(), registrar.messenger());
     }
 
-    if (enabled) {
-      oneLoggingConfiguration = OneLoggingConfiguration.builder()
-              .log(OneLogLevel.VERBOSE)
-              .log(OneLogLevel.DEBUG)
-              .log(OneLogComponent.ANY)
-              .build();
-    } else {
-      oneLoggingConfiguration = OneLoggingConfiguration.builder()
-              .log(OneLogLevel.WARN)
-              .log(OneLogLevel.ERROR)
-              .log(OneLogComponent.ANY)
-              .build();
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
     }
 
-    One.setLoggingConfiguration(oneLoggingConfiguration);
-    result.success("Set Thunderhead LogLevel: " + (enabled ? "Enabled" : "Disabled"));
-  }
+    private void onAttachedToEngine(Context applicationContext, BinaryMessenger messenger) {
+        methodChannel = new MethodChannel(messenger, "one_sdk_flutter");
+        methodChannel.setMethodCallHandler(this);
+    }
 
-  private void sendInteraction(String interactionPath, HashMap properties, final Result result) {
-    final OneRequest sendInteractionRequest = new OneRequest.Builder()
-            .interactionPath(new OneInteractionPath(URI.create(interactionPath)))
-            .properties(properties)
-            .build();
-    executor.submit(() -> {
-      OneResponse response;
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-         try {
-        response = One.sendInteraction(sendInteractionRequest).join();
-        HashMap<String, Object> responseMap = responseObjectToHashMap(response);
-          result.success(responseMap);
-        } catch (ExecutionException error) {
-          result.error(Integer.toString(error.hashCode()), error.getLocalizedMessage(), error);
-          Log.e(LOG_TAG, "Completion Error: " + error.getCause());
-        } catch (OneSDKError error) {
-          result.error(Integer.toString(error.getSystemCode()), error.getLocalizedMessage(), error);
-          Log.e(LOG_TAG, "SDK Error: " + error.getErrorMessage());
-        } catch (OneAPIError error) {
-          result.error(Integer.toString(error.getHttpStatusCode()), error.getLocalizedMessage(), error);
-          Log.e(LOG_TAG, "Api Error: " + error.getErrorMessage());
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        methodChannel.setMethodCallHandler(null);
+        methodChannel = null;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("sendInteraction")) {
+            final Map<String, Object> arguments = call.arguments();
+            String interactionPath = (String) arguments.get("interactionPath");
+            HashMap<String, Object> properties = (HashMap) arguments.get("properties");
+            try {
+                sendInteraction(interactionPath, properties, result);
+            } catch (Exception e) {
+                result.error("Failed to send interaction", e.getLocalizedMessage(), e);
+            }
+        } else if (call.method.equals("sendResponseCode")) {
+            final Map<String, String> arguments = call.arguments();
+            String responseCode = (String) arguments.get("responseCode");
+            String interactionPath = (String) arguments.get("interactionPath");
+            sendResponseCode(responseCode, interactionPath, result);
+        } else if (call.method.equals("setLogLevel")) {
+            setLogLevel(call, result);
+        } else if (call.method.equals("initializeOne")) {
+            initializeOne(call, result);
+        } else if (call.method.equals("optOut")) {
+            optOut(call, result);
+        } else {
+            result.notImplemented();
         }
-      } else {
-        try {
-          response = One.sendInteractionLegacySupport(sendInteractionRequest).join();
-          HashMap<String, Object> responseMap = responseObjectToHashMap(response);
-          result.success(responseMap);
-        } catch (ExecutionException error) {
-          result.error(Integer.toString(error.hashCode()), error.getLocalizedMessage(), error);
-          Log.e(LOG_TAG, "Execution Exception Error: " + error.getLocalizedMessage());
-          error.printStackTrace();
-        }
-      }
-    });
-  }
-
-  private void sendResponseCode(String responseCode, String interactionPath, final Result result) {
-    final OneResponseCodeRequest responseCodeRequest = new OneResponseCodeRequest.Builder()
-    .responseCode(new OneResponseCode(responseCode))
-    .interactionPath(new OneInteractionPath(URI.create(interactionPath)))
-    .build();
-
-    try {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        One.sendResponseCode(responseCodeRequest);
-      } else {
-        One.sendResponseCodeLegacySupport(responseCodeRequest);
-      }
-    } catch (ExecutionException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void optOut(MethodCall call, final Result result) {
-    OneOptOutConfiguration optOutConfiguration;
-    Set<OneOptInOptions> optInOptions = EnumSet.noneOf(OneOptInOptions.class);
-
-    final HashMap<String, Object> arguments = call.arguments();
-    Boolean optOut = (Boolean) arguments.get("optOut");
-    List<String> options = (List<String>) arguments.get("options");
-
-    if (optOut == null) {
-      optOut = false;
     }
 
-    if (options != null) {
-      for (String optOutOption : options) {
-        if (optOutOption == "cityCountryDetection") {
-          optInOptions.add(OneOptInOptions.CITY_COUNTRY_DETECTION);
-        }
-      }
+    private void initializeOne(MethodCall call, final Result result) {
+        final Map<String, Object> arguments = call.arguments();
+        String siteKey = (String) arguments.get("siteKey");
+        String touchpointURI = (String) arguments.get("touchpoint");
+        String apiKey = (String) arguments.get("apiKey");
+        String sharedSecret = (String) arguments.get("sharedSecret");
+        String userId = (String) arguments.get("userID");
+        String host = (String) arguments.get("host");
+        boolean adminMode = (Boolean) arguments.get("adminMode");
+
+        final OneConfiguration oneConfiguration = new OneConfiguration.Builder()
+                .siteKey(siteKey)
+                .apiKey(apiKey)
+                .sharedSecret(sharedSecret)
+                .userId(userId)
+                .host(URI.create(host))
+                .touchpoint(URI.create(touchpointURI))
+                .mode(adminMode ? OneMode.ADMIN : OneMode.USER)
+                .build();
+        One.setConfiguration(oneConfiguration);
+        result.success(true);
     }
 
-    optOutConfiguration = new OneOptOutConfiguration.Builder()
-            .optOut(optOut)
-            .optInOptions(optInOptions)
-            .build();
-    One.setOptOutConfiguration(optOutConfiguration);
+    private void setLogLevel(MethodCall call, final Result result) {
+        OneLoggingConfiguration oneLoggingConfiguration;
+        final Map<String, Boolean> arguments = call.arguments();
+        Boolean enabled = arguments.get("logLevel");
 
-    result.success("Set Thunderhead OptOut: " + (optOut ? "Opted out" : "Opted in"));
-  }
-
-  // Helper methods
-
-  private HashMap<String, Object> responseObjectToHashMap(OneResponse response) {
-      // Convert Class object to HashMap so Dart can read it.
-      HashMap<String, Object> responseMap = new HashMap<>();
-      responseMap.put("tid", response.getTid());
-      responseMap.put("interactionPath", response.getInteractionPath().getValue().getPath());
-
-      if (!response.getOptimizationPoints().isEmpty()) {
-        List<HashMap<String, Object>> optimizationsList = new ArrayList<>();
-
-        HashMap<String, Object> optimizationPointMap = new HashMap<>();
-
-        for (OptimizationPoint point : response.getOptimizationPoints()) {
-          optimizationPointMap.put("data", point.getData());
-          optimizationPointMap.put("path", point.getPath());
-          optimizationPointMap.put("responseId", point.getResponseId());
-          optimizationPointMap.put("dataMimeType", point.getDataMimeType());
-          optimizationPointMap.put("directives", point.getDirectives());
-          optimizationPointMap.put("name", point.getName());
-          optimizationPointMap.put("viewPointName", point.getViewPointName());
-          optimizationPointMap.put("viewPointId", point.getViewPointId());
-          optimizationsList.add(optimizationPointMap);
+        if (enabled == null) {
+            enabled = true;
         }
 
-        responseMap.put("optimizationPoints",  optimizationsList);
-      }
-      return responseMap;
-  }
+        if (enabled) {
+            oneLoggingConfiguration = OneLoggingConfiguration.builder()
+                    .log(OneLogLevel.VERBOSE)
+                    .log(OneLogLevel.DEBUG)
+                    .log(OneLogComponent.ANY)
+                    .build();
+        } else {
+            oneLoggingConfiguration = OneLoggingConfiguration.builder()
+                    .log(OneLogLevel.WARN)
+                    .log(OneLogLevel.ERROR)
+                    .log(OneLogComponent.ANY)
+                    .build();
+        }
 
+        One.setLoggingConfiguration(oneLoggingConfiguration);
+        result.success("Set Thunderhead LogLevel: " + (enabled ? "Enabled" : "Disabled"));
+    }
+
+    private void sendInteraction(String interactionPath, HashMap properties, final Result result) {
+        final OneRequest sendInteractionRequest = new OneRequest.Builder()
+                .interactionPath(new OneInteractionPath(URI.create(interactionPath)))
+                .properties(properties)
+                .build();
+        executor.submit(() -> {
+            OneResponse response;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                try {
+                    response = One.sendInteraction(sendInteractionRequest).join();
+                    HashMap<String, Object> responseMap = responseObjectToHashMap(response);
+                    result.success(responseMap);
+                } catch (ExecutionException error) {
+                    result.error(Integer.toString(error.hashCode()), error.getLocalizedMessage(), error);
+                    Log.e(LOG_TAG, "Completion Error: " + error.getCause());
+                } catch (OneSDKError error) {
+                    result.error(Integer.toString(error.getSystemCode()), error.getLocalizedMessage(), error);
+                    Log.e(LOG_TAG, "SDK Error: " + error.getErrorMessage());
+                } catch (OneAPIError error) {
+                    result.error(Integer.toString(error.getHttpStatusCode()), error.getLocalizedMessage(), error);
+                    Log.e(LOG_TAG, "Api Error: " + error.getErrorMessage());
+                }
+            } else {
+                try {
+                    response = One.sendInteractionLegacySupport(sendInteractionRequest).join();
+                    HashMap<String, Object> responseMap = responseObjectToHashMap(response);
+                    result.success(responseMap);
+                } catch (ExecutionException error) {
+                    result.error(Integer.toString(error.hashCode()), error.getLocalizedMessage(), error);
+                    Log.e(LOG_TAG, "Execution Exception Error: " + error.getLocalizedMessage());
+                    error.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void sendResponseCode(String responseCode, String interactionPath, final Result result) {
+        executor.submit(() -> {
+            final OneResponseCodeRequest responseCodeRequest = new OneResponseCodeRequest.Builder()
+                    .responseCode(new OneResponseCode(responseCode))
+                    .interactionPath(new OneInteractionPath(URI.create(interactionPath)))
+                    .build();
+
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    One.sendResponseCode(responseCodeRequest);
+                } else {
+                    One.sendResponseCodeLegacySupport(responseCodeRequest);
+                }
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void optOut(MethodCall call, final Result result) {
+        OneOptOutConfiguration optOutConfiguration;
+        Set<OneOptInOptions> optInOptions = EnumSet.noneOf(OneOptInOptions.class);
+
+        final HashMap<String, Object> arguments = call.arguments();
+        Boolean optOut = (Boolean) arguments.get("optOut");
+        List<String> options = (List<String>) arguments.get("options");
+
+        if (optOut == null) {
+            optOut = false;
+        }
+
+        if (options != null) {
+            for (String optOutOption : options) {
+                if (optOutOption == "cityCountryDetection") {
+                    optInOptions.add(OneOptInOptions.CITY_COUNTRY_DETECTION);
+                }
+            }
+        }
+
+        optOutConfiguration = new OneOptOutConfiguration.Builder()
+                .optOut(optOut)
+                .optInOptions(optInOptions)
+                .build();
+        One.setOptOutConfiguration(optOutConfiguration);
+
+        result.success("Set Thunderhead OptOut: " + (optOut ? "Opted out" : "Opted in"));
+    }
+
+    // Helper methods
+
+    // Convert OneResponse Class to HashMap so Dart can read it.
+    private HashMap<String, Object> responseObjectToHashMap(OneResponse response) {
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        responseMap.put("tid", response.getTid());
+        responseMap.put("interactionPath", response.getInteractionPath().getValue().getPath());
+
+        if (!response.getOptimizationPoints().isEmpty()) {
+            List<HashMap<String, Object>> optimizationsList = new ArrayList<>();
+            HashMap<String, Object> optimizationPointMap = new HashMap<>();
+
+            for (OptimizationPoint point : response.getOptimizationPoints()) {
+                optimizationPointMap.put("data", point.getData());
+                optimizationPointMap.put("path", point.getPath());
+                optimizationPointMap.put("responseId", point.getResponseId());
+                optimizationPointMap.put("dataMimeType", point.getDataMimeType());
+                optimizationPointMap.put("directives", point.getDirectives());
+                optimizationPointMap.put("name", point.getName());
+                optimizationPointMap.put("viewPointName", point.getViewPointName());
+                optimizationPointMap.put("viewPointId", point.getViewPointId());
+                optimizationsList.add(optimizationPointMap);
+            }
+
+            responseMap.put("optimizationPoints", optimizationsList);
+        }
+        return responseMap;
+    }
 }
